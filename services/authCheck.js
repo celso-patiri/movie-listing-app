6fee3b9b8a68da089d331ffd95e40c5c4282f4f8
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-02-27 19:07:39 -0300

    removed bootstrap

--- services/authCheck.js
+++ services/authCheck.js
@@ -1,14 +1,14 @@
 module.exports = {
-    checkAuthenticated: function checkAuthenticated(req, res, next){
-        if(req.isAuthenticated()){
-            return next();
-        }
-        res.redirect('/login')
-    },
-    checkNotAuthenticated: function checkNotAuthenticated(req, res, next){
-        if(req.isAuthenticated()){
-            return res.redirect('/')
-        }
-        next()
-    }
-}
+	checkAuthenticated: function checkAuthenticated(req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		}
+		res.redirect('/login');
+	},
+	checkNotAuthenticated: function checkNotAuthenticated(req, res, next) {
+		if (req.isAuthenticated()) {
+			return res.redirect('/');
+		}
+		next();
+	},
+};
